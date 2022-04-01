094386b2af8a99bcfef199c25a2a433a08c6690e
Christian Halvorson <christianphalv@gmail.com>
2022-04-01 16:44:20 -0500

    Initial commit

---
+++ GraphicsEngine/GraphicsEngine.cpp
@@ -0,0 +1,12 @@
+﻿// GraphicsEngine.cpp : Defines the entry point for the application.
+//
+
+#include "GraphicsEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
